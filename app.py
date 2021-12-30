9ab1c2cfa71b095e502bdf8aaa6bd1313da95ac4
Abiola Olatunbosun <72774774+bosunolaide@users.noreply.github.com>
2021-12-30 02:05:47 +0100

    updated app.py file

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ classifier = pickle.load(pickle_in)
 @st.cache()
   
 # defining the function which will make the prediction using the data which the user inputs 
-def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
+def prediction(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome,  LoanAmount, Credit_History, Property_Area):   
  
     # Pre-processing user input    
-    if Gender == "Male":
+    if Gender == "Female":
         Gender = 0
     else:
         Gender = 1
@@ -20,17 +20,41 @@ def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):
         Married = 0
     else:
         Married = 1
+        
+    if Dependents == "0":
+        Dependents = 0
+    elif Dependents == "1":
+        Dependents = 1
+    elif Dependents == "2":
+        Dependents = 2
+    else:
+        Dependents = 3
  
-    if Credit_History == "Unclear Debts":
+    if Education == "Undergraduate":
+        Education = 0
+    else:
+        Education = 1
+        
+    if Self_Employed == "Not Self Employed":
+        Self_Employed = 0
+    else:
+        Self_Employed = 1
+        
+    if Property_Area == "Rural Area":
+        Property_Area = 0
+    elif Property_Area == "Semi-Urban Area":
+        Property_Area = 1
+    else:
+        Property_Area = 2
+        
+    if Credit_History == "Uncleared Debts":
         Credit_History = 0
     else:
         Credit_History = 1  
  
-    LoanAmount = LoanAmount / 1000
- 
     # Making predictions 
     prediction = classifier.predict( 
-        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+        [[Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome,  LoanAmount, Credit_History, Property_Area]])
      
     if prediction == 0:
         pred = 'Rejected'
@@ -54,14 +78,19 @@ def main():
     # following lines create boxes in which user can enter data required to make prediction 
     Gender = st.selectbox('Gender',("Male","Female"))
     Married = st.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = st.number_input("Applicants monthly income") 
-    LoanAmount = st.number_input("Total loan amount")
-    Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
+    Dependents = st.selectbox('Number of Dependents',("0","1","2","3+"))
+    Education = st.selectbox('Education',("Undergraduate","Graduate")) 
+    Self_Employed = st.selectbox('Employment Status',("Not Self Employed","Self Employed")) 
+    ApplicantIncome = st.number_input("Applicant's Monthly income") 
+    CoapplicantIncome = st.number_input("Co-Applicant's Monthly Income")
+    LoanAmount = st.number_input("Total Loan Amount")
+    Credit_History = st.selectbox('Credit_History',("Uncleared Debts","No Uncleared Debts"))
+    Property_Area = st.selectbox('Property Location Area',("Rural Area","Semi-Urban Area", "Urban Area")) 
     result =""
       
     # when 'Predict' is clicked, make the prediction and store it 
     if st.button("Predict"): 
-        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
+        result = prediction(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome,  LoanAmount, Credit_History, Property_Area) 
         st.success('Your loan is {}'.format(result))
         print(LoanAmount)
      
